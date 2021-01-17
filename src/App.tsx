e4affa301c8d91028f1112991c7f1d17117fa3dd
lsdsjy <lsdsjy@163.com>
2021-01-17 23:43:08 +0800

    feat: simple undo/redo

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,41 @@ import React, { useState } from 'react'
 import './App.css'
 import { MindMap } from './components/Map'
 import mockRoot from './mock'
-import { ViewRoot } from './model'
+import { Root } from './model'
+
+let undoStack: Root[] = []
+let redoStack: Root[] = []
 
 function App() {
-  const [root, setRoot] = useState(mockRoot as ViewRoot)
+  const [root, setRoot] = useState(mockRoot as Root)
+
+  function update(newRoot: Root) {
+    undoStack.push(root)
+    redoStack = []
+    setRoot(newRoot)
+  }
+
+  function undo() {
+    const last = undoStack.pop()
+    if (last) {
+      redoStack.push(root)
+      setRoot(last)
+    }
+  }
+
+  function redo() {
+    const next = redoStack.pop()
+    if (next) {
+      undoStack.push(root)
+      setRoot(next)
+    }
+  }
+
   return (
     <div className="App">
-      <MindMap root={root} onChange={setRoot as any} />
+      <button onClick={undo}>undo</button>
+      <button onClick={redo}>redo</button>
+      <MindMap root={root} onChange={update} />
     </div>
   )
 }
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,12 +3,12 @@ import { Root, ViewNode } from '../model'
 import { layOutRoot } from '../util/layout'
 import { Node } from './Node'
 
-export function MindMap(props: { root: Root; onChange: (root: ViewNode) => void }) {
+export function MindMap(props: { root: Root; onChange: (root: Root) => void }) {
   const { root } = props
   const viewRoot = useMemo(() => layOutRoot(root), [root])
   return (
     <div style={{ transform: 'translate(300px, 300px)' }}>
-      <Node onCreateSibling={() => {}} node={viewRoot} onChange={(newRoot) => props.onChange(newRoot)} />
+      <Node onCreateSibling={() => {}} node={viewRoot} onChange={(newRoot) => props.onChange(newRoot as Root)} />
     </div>
   )
 }
