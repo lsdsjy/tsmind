4291acf532f1cb8f8b06d2279c7aa9e9c37b0906
lsdsjy <lsdsjy@163.com>
2021-01-17 23:01:57 +0800

    fix: deploy public path

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  build: {
+    base: '/tsmind/'
+  },
   plugins: [reactRefresh()]
 })
