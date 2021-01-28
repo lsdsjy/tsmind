9f471c0393f73be926dbeb3ac2b0981c4eb7f7e3
lsdsjy <yangyl04@fenbi.com>
2021-01-28 18:14:20 +0800

    chore: upgrade vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  build: {
-    base: '/tsmind/'
-  },
+  base: '/tsmind/',
   optimizeDeps: {
     include: ['rxjs/operators']
   },
